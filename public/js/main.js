e30b4a975c0a4dc1f5cc72c908e4b5134e7a43b1
Juha Tauriainen <juha@bin.fi>
2013-04-21 00:06:39 +0300

    jQuery update

--- public/js/main.js
+++ public/js/main.js
@@ -3,7 +3,7 @@
     baseUrl: "/js",
     enforceDefine: true,
     paths: {
-      jquery: "http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min",
+      jquery: "http://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min",
       backbone: "libs/backbone",
       underscore: "libs/underscore",
       text: "libs/text",
